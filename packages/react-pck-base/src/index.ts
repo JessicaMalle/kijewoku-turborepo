7b7b317b842ad1c0a2b4eeb00df349df471c8cfe
Jessica Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-01-03 14:11:42 +0100

    build: add styled-component

--- packages/react-pck-base/src/index.ts
+++ packages/react-pck-base/src/index.ts
@@ -1,1 +1,9 @@
+'use client'
 // Use the "generate:component" command to gen code (cf. turbo/generators/config.cjs)
+import styled from 'styled-components';
+
+export {default as Salut} from './Salut.tsx';
+
+export const Button = styled.button`
+    background-color: hotpink;
+`;
