37d6ea51797abba5bf68653c88eaa8de306bba83
Jessica Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-01-03 14:13:47 +0100

    chore: clean packages

--- packages/react-pkg-base/src/index.ts
+++ packages/react-pkg-base/src/index.ts
@@ -1,8 +1,1 @@
 // Use the "generate:component" command to gen code (cf. turbo/generators/config.cjs)
-import styled from 'styled-components';
-
-export {default as Salut} from './Salut.tsx';
-
-export const Button = styled.button`
-    background-color: hotpink;
-`;
