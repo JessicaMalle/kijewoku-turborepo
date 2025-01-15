2b2aebde4fa0f65fd19b0516d8ab943a6dae8c4b
Jessica Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-01-15 08:23:11 +0100

    chore: update moduleResolution to 'bundler' in all packages and templates

--- packages/hooks/src/screen/use-screen-orientation.tsx
+++ packages/hooks/src/screen/use-screen-orientation.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function useScreenOrientation(): 'portrait' | 'landscape' {
   const [orientation, setOrientation] = useState<'portrait' | 'landscape'>(
