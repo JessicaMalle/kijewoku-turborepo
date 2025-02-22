671a5981334f264e1b7126f40691e36a538871e6
Jessica Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-02-22 20:48:28 +0100

    feat: can't draw more of 7 cards

--- apps/poker-tap/src/services/GameService.ts
+++ apps/poker-tap/src/services/GameService.ts
@@ -3,6 +3,15 @@ import DeckService from "./DeckService";
 import HandService from "./HandService";
 
 const drawCardAndDeductChips = (deck: Deck, hand: Hand, currentChips: number): { deck: Deck, hand: Hand, remainingChips: number } => {
+  if (hand.handCards.length === 7) {
+    console.error("Can't draw 8 or more cards");
+    return {
+      deck,
+      hand,
+      remainingChips: currentChips,
+    }
+  }
+
   const cardPrice = DeckService.nextCardPrice(deck);
   if (currentChips < cardPrice) {
     console.error("Not enough chips to draw a card");
