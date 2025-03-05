a150be6ee20c591fd0a4ac35b330c4d4e34dd791
Jessica Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-03-05 08:43:36 +0100

    feat: add useAnimation custom hook

---
+++ packages/hooks/src/animation/useAnimation.ts
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+
+    interface AnimationOptions {
+      keyframes: Keyframe[];
+      options: KeyframeAnimationOptions;
+      eventType?: keyof HTMLElementEventMap;
+    }
+
+    const useAnimation = ({ keyframes, options, eventType }: AnimationOptions) => {
+      const elementRef = useRef<HTMLDivElement | null>(null);
+
+      useEffect(() => {
+        const element = elementRef.current;
+        if (!element) return;
+
+        const startAnimation = () => {
+          const animation = element.animate(keyframes, options);
+          return () => animation.cancel();
+        };
+
+        if (eventType) {
+          const handleEvent = (event: Event) => startAnimation();
+          element.addEventListener(eventType, handleEvent);
+          return () => element.removeEventListener(eventType, handleEvent);
+        }
+          startAnimation();
+      }, [keyframes, options, eventType]);
+
+      return elementRef;
+    };
+
+    export default useAnimation;
