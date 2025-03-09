706711bbb994da1dd7dc42711adc84afda980103
Jessica Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-03-09 10:09:55 +0100

    feat: update styling and fonts for poker tap
    Updated card styles to include the "Bebas Neue" font. Modified the app title and added Google Fonts for improved text presentation. Adjusted body font-family to use "Poppins".

--- apps/poker-tap/src/components/Card/Card.styles.ts
+++ apps/poker-tap/src/components/Card/Card.styles.ts
@@ -1,27 +1,28 @@
 import styled from "styled-components";
 
 export const StyledCard = styled.div<{ color: string; active?: string }>`
-  width: 100px;
-  min-width: 100px;
-  height: 150px;
-  border: 1px solid #000;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 10px;
-  background-color: white;
-  color: ${props => (props.color === 'hearts' || props.color === 'diamonds') ? 'red' : 'black'};
-  box-shadow: ${props => props.active ? '0 0 10px 5px rgba(0, 0, 255, 0.5)' : 'none'};
-  transform: ${props => props.active ? 'scale(1.1)' : 'scale(1)'};
-  transition: transform 0.2s, box-shadow 0.2s;
+    width: 100px;
+    min-width: 100px;
+    height: 150px;
+    border: 1px solid #000;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 10px;
+    background-color: white;
+    color: ${props => (props.color === 'hearts' || props.color === 'diamonds') ? 'red' : 'black'};
+    box-shadow: ${props => props.active ? '0 0 10px 5px rgba(0, 0, 255, 0.5)' : 'none'};
+    transform: ${props => props.active ? 'scale(1.1)' : 'scale(1)'};
+    transition: transform 0.2s, box-shadow 0.2s;
+    font-family: "Bebas Neue", sans-serif
 `;
 
 export const CardValue = styled.div`
-  font-size: 1.5em;
+    font-size: 1.5em;
 `;
 
 export const CardSuit = styled.div`
-  font-size: 2em;
-  text-align: center;
+    font-size: 2em;
+    text-align: center;
 `;
