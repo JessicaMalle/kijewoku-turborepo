c0596b3fc6c5cbe82e8b73d508da91d4da2a08ce
J. Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-04-17 13:11:12 +0200

    chore: adjust Section div width for neutral style to use 'auto'

--- apps/poker-tap/src/components/Layouts/Section.styles.ts
+++ apps/poker-tap/src/components/Layouts/Section.styles.ts
@@ -6,7 +6,7 @@ export const StyledSection = styled.section<{ $neutralStyle?: boolean }>`
     border: ${({ $neutralStyle }) => ($neutralStyle ? "0" : `3px solid ${colors.neutrals.dark}`)};
     border-radius: ${({ $neutralStyle }) => ($neutralStyle ? "0" : "6px")};
 		> div {
-				width: ${({ $neutralStyle }) => ($neutralStyle ? "100%" : "calc(100% - 2px - 30px)")};
+				width: ${({ $neutralStyle }) => ($neutralStyle ? "auto" : "calc(100% - 2px - 30px)")};
         height: ${({ $neutralStyle }) => ($neutralStyle ? "auto" : "calc(100% - 30px - 2px - 5px)")};
         padding: ${({ $neutralStyle }) => ($neutralStyle ? "0" : "15px")};
         background-color: ${({ $neutralStyle }) => ($neutralStyle ? "transparent" : colors.blues.darkBlue)};
