a18fff33b2b77e3c6b378b30505cb60ed29cc9da
J. Malle <30345089+JessicaMalle@users.noreply.github.com>
2025-06-10 12:59:26 +0200

    feat: add Booster interface to define game card booster packs

--- apps/poker-tap/src/types/gameTypes.ts
+++ apps/poker-tap/src/types/gameTypes.ts
@@ -37,6 +37,13 @@ export interface Deck {
 	cards: Card[];
 }
 
+export interface Booster {
+	cards: Card[];
+	edition: "basic"; // the first edition is called "Basic Edition"
+	type: "gameCard"; // later we have to add some new type of cards in the game
+	size: 3 | 5 | 7; // affect the price and/or the rarity of the booster
+}
+
 export interface Combination {
 	uid: string;
 	bonusValue: number;
